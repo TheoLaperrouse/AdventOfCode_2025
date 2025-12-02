1a1a75c39ca1edbae5bfa6725f06e198acecf619
Théo Laperrouse <theo.laperrouse@energiency.com>
2025-12-02 01:00:02 +0100

    Day 1

--- src/days/day01.ts
+++ src/days/day01.ts
@@ -1,17 +1,49 @@
 export function part1(input: string) {
     const _lines = input.trim().split("\n");
+    let sum = 50;
+    let count = 0;
+    for (const line of _lines) {
+        const [dir, strNumber] = [line[0], line.slice(1)];
+        const number = parseInt(strNumber);
 
-    // TODO
+        sum += dir === "L" ? -number : number;
+        sum %= 100;
 
-    return 0;
+        if (sum === 0) {
+            count++;
+        }
+    }
+    return count;
 }
 
 export function part2(input: string) {
     const _lines = input.trim().split("\n");
+    let sum = 50;
+    let count = 0;
+    for (const line of _lines) {
+        const countAdd = sum === 0 ? 0 : 1;
+        const [dir, strNumber] = [line[0], line.slice(1)];
 
-    // TODO
+        const number = parseInt(strNumber);
 
-    return 0;
+        const fullTurns = Math.abs(number / 100) | 0;
+        count += fullTurns;
+
+        const toAdd = number % 100;
+
+        sum += dir === "L" ? -toAdd : toAdd;
+
+        if (sum > 99) {
+            sum -= 100;
+            count += countAdd;
+        } else if (sum < 0) {
+            sum += 100;
+            count += countAdd;
+        } else if (sum === 0) {
+            count += countAdd;
+        }
+    }
+    return count;
 }
 
 export default function (input: string) {
